5765666fd90d45a3e5ce43e15036040e1a21e601
Chris Rood <crood58@gmail.com>
2021-10-10 16:15:49 -0400

    Removed whitespaces

--- quart_bcrypt.py
+++ quart_bcrypt.py
@@ -1,7 +1,7 @@
 '''
-    quartext.bcrypt
-    ---------------
-    A Quart extension providing bcrypt hashing and comparison facilities.
+quartext.bcrypt
+---------------
+A Quart extension providing bcrypt hashing and comparison facilities.
     : Orginal Code taken from Flask Bcrypt https://github.com/maxcountryman/flask-bcrypt
 '''
 
@@ -28,11 +28,11 @@ import hashlib
 from quart import Quart
 
 def generate_password_hash(password, rounds=None):
-    '''This helper function wraps the eponymous method of :class:`Bcrypt`. It 
-    is intended to be used as a helper function at the expense of the 
-    configuration variable provided when passing back the app object. In other 
+    '''This helper function wraps the eponymous method of :class:`Bcrypt`. It
+    is intended to be used as a helper function at the expense of the
+    configuration variable provided when passing back the app object. In other
     words this shortcut does not make use of the app object at all.
-    To use this function, simply import it from the module and use it in a 
+    To use this function, simply import it from the module and use it in a
     similar fashion as the original method would be used. Here is a quick
     example::
         from quart_bcrypt import generate_password_hash
@@ -44,11 +44,11 @@ def generate_password_hash(password, rounds=None):
 
 
 def check_password_hash(pw_hash, password):
-    '''This helper function wraps the eponymous method of :class:`Bcrypt.` It 
-    is intended to be used as a helper function at the expense of the 
-    configuration variable provided when passing back the app object. In other 
+    '''This helper function wraps the eponymous method of :class:`Bcrypt.` It
+    is intended to be used as a helper function at the expense of the
+    configuration variable provided when passing back the app object. In other
     words this shortcut does not make use of the app object at all.
-    To use this function, simply import it from the module and use it in a 
+    To use this function, simply import it from the module and use it in a
     similar fashion as the original method would be used. Here is a quick
     example::
         from quart_bcrypt import check_password_hash
@@ -60,41 +60,41 @@ def check_password_hash(pw_hash, password):
 
 
 class Bcrypt(object):
-    '''Bcrypt class container for password hashing and checking logic using 
-    bcrypt, of course. This class may be used to intialize your Quart app 
-    object. The purpose is to provide a simple interface for overriding 
+    '''Bcrypt class container for password hashing and checking logic using
+    bcrypt, of course. This class may be used to intialize your Quart app
+    object. The purpose is to provide a simple interface for overriding
     Werkzeug's built-in password hashing utilities.
-    Although such methods are not actually overriden, the API is intentionally 
-    made similar so that existing applications which make use of the previous 
-    hashing functions might be easily adapted to the stronger facility of 
+    Although such methods are not actually overriden, the API is intentionally
+    made similar so that existing applications which make use of the previous
+    hashing functions might be easily adapted to the stronger facility of
     bcrypt.
-    To get started you will wrap your application's app object something like 
+    To get started you will wrap your application's app object something like
     this::
         app = Quart(__name__)
         bcrypt = Bcrypt(app)
     Now the two primary utility methods are exposed via this object, `bcrypt`.
-    So in the context of the application, important data, such as passwords, 
+    So in the context of the application, important data, such as passwords,
     could be hashed using this syntax::
         password = 'hunter2'
         pw_hash = bcrypt.generate_password_hash(password)
-    Once hashed, the value is irreversible. However in the case of validating 
-    logins a simple hashing of candidate password and subsequent comparison. 
-    Importantly a comparison should be done in constant time. This helps 
+    Once hashed, the value is irreversible. However in the case of validating
+    logins a simple hashing of candidate password and subsequent comparison.
+    Importantly a comparison should be done in constant time. This helps
     prevent timing attacks. A simple utility method is provided for this::
         candidate = 'secret'
         bcrypt.check_password_hash(pw_hash, candidate)
-    If both the candidate and the existing password hash are a match 
+    If both the candidate and the existing password hash are a match
     `check_password_hash` returns True. Otherwise, it returns False.
-    .. admonition:: Namespacing Issues 
-        It's worth noting that if you use the format, `bcrypt = Bcrypt(app)` 
-        you are effectively overriding the bcrypt module. Though it's unlikely 
-        you would need to access the module outside of the scope of the 
+    .. admonition:: Namespacing Issues
+        It's worth noting that if you use the format, `bcrypt = Bcrypt(app)`
+        you are effectively overriding the bcrypt module. Though it's unlikely
+        you would need to access the module outside of the scope of the
         extension be aware that it's overriden.
-        Alternatively consider using a different name, such as `flask_bcrypt 
+        Alternatively consider using a different name, such as `flask_bcrypt
         = Bcrypt(app)` to prevent naming collisions.
-    Additionally a configuration value for `BCRYPT_LOG_ROUNDS` may be set in 
-    the configuration of the Flask app. If none is provided this will 
-    internally be assigned to 12. (This value is used in determining the 
+    Additionally a configuration value for `BCRYPT_LOG_ROUNDS` may be set in
+    the configuration of the Flask app. If none is provided this will
+    internally be assigned to 12. (This value is used in determining the
     complexity of the encryption, see bcrypt for more details.)
     You may also set the hash version using the `BCRYPT_HASH_PREFIX` field in
     the configuration of the Flask app. If not set, this will default to `2b`.
@@ -144,9 +144,9 @@ class Bcrypt(object):
         '''Generates a password hash using bcrypt. Specifying `rounds`
         sets the log_rounds parameter of `bcrypt.gensalt()` which determines
         the complexity of the salt. 12 is the default value. Specifying `prefix`
-        sets the `prefix` parameter of `bcrypt.gensalt()` which determines the 
+        sets the `prefix` parameter of `bcrypt.gensalt()` which determines the
         version of the algorithm used to create the hash.
-        Example usage of :class:`generate_password_hash` might look something 
+        Example usage of :class:`generate_password_hash` might look something
         like this::
             pw_hash = bcrypt.generate_password_hash('secret', 10)
         :param password: The password to be hashed.
@@ -161,7 +161,6 @@ class Bcrypt(object):
             rounds = self._log_rounds
         if prefix is None:
             prefix = self._prefix
-        
         # Python 3 unicode strings must be encoded as bytes before hashing.
         password = self._unicode_to_bytes(password)
         prefix = self._unicode_to_bytes(prefix)
@@ -174,10 +173,10 @@ class Bcrypt(object):
         return bcrypt.hashpw(password, salt)
 
     def check_password_hash(self, pw_hash, password):
-        '''Tests a password hash against a candidate password. The candidate 
-        password is first hashed and then subsequently compared in constant 
+        '''Tests a password hash against a candidate password. The candidate
+        password is first hashed and then subsequently compared in constant
         time to the existing hash. This will either return `True` or `False`.
-        Example usage of :class:`check_password_hash` would look something 
+        Example usage of :class:`check_password_hash` would look something
         like this::
             pw_hash = bcrypt.generate_password_hash('secret', 10)
             bcrypt.check_password_hash(pw_hash, 'secret') # returns True
@@ -192,5 +191,5 @@ class Bcrypt(object):
         if self._handle_long_passwords:
             password = hashlib.sha256(password).hexdigest()
             password = self._unicode_to_bytes(password)
-
+            
         return hmac.compare_digest(bcrypt.hashpw(password, pw_hash), pw_hash)
