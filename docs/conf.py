2c6bae3712fd8ba78dcb729df8617419fabb4a4f
Chris Rood <crood58@gmail.com>
2021-10-10 14:25:12 -0400

    Updated

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(os.path.join(os.path.dirname(__file__), '../quart_bcrypt.py'))
 
 module_path = os.path.join(os.path.dirname(__file__), '../quart_bcrypt.py')
 
