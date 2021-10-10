fc2c208d0572330b1b630ab33b9c4cda3bd3f9b6
Chris Rood <crood58@gmail.com>
2021-10-10 16:25:57 -0400

    Updated sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath('../'))
 
 module_path = os.path.join(os.path.dirname(__file__), '../quart_bcrypt.py')
