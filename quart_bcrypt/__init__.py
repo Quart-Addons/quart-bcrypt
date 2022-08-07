041dbbe64df2a3ad5e839e2edba01c7cbbd9c263
Chris Rood <crood58@gmail.com>
2022-08-07 19:52:10 +0000

    Moved to package.

---
+++ quart_bcrypt/__init__.py
@@ -0,0 +1,21 @@
+"""
+Quart Bcrypt
+
+A Quart extension providing bcrypt hashing and comparison facilities.
+    Code is based on flask_bcrypt https://github.com/maxcountryman/flask-bcrypt
+
+    :copyright: (c) 2022 by Chris Rood.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from .core import Bcrypt
+from .helpers import (generate_password_hash, check_password_hash,
+                     async_generate_password_hash, async_check_password_hash)
+
+__all__ = (
+    'Bcrypt',
+    'generate_password_hash',
+    'check_password_hash',
+    'async_generate_password_hash',
+    'async_check_password_hash'
+)
